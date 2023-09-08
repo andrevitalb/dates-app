13f9e595d76c32f3530bee661ed0b22ac9733cc1
André Vital <andre@andrevital.com>
2023-09-08 10:03:57 -0600

    feat: FE first API implementation (#9)
    * Updated base html component to map API data
    
    * Add HTTP Client module to app module
    
    * Add API data fetch onInit

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+	title = "Dates App"
+	users: any
+
+	constructor(private http: HttpClient) {}
+
+	ngOnInit(): void {
+		this.http.get("https://localhost:5001/api/users").subscribe({
+			next: (response) => (this.users = response),
+			error: (err) => console.error(err),
+			complete: () => console.log("Request completed!"),
+		})
+	}
 }
