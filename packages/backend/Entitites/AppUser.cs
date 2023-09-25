0f978efbe2972c7ef87e251107691b53dbf84e6a
André Vital <andre@andrevital.com>
2023-09-25 00:24:35 -0600

    Added password hash/salt to AppUser

--- packages/backend/Entitites/AppUser.cs
+++ packages/backend/Entitites/AppUser.cs
@@ -2,4 +2,7 @@ namespace backend.Entities;
 public class AppUser {
   public int Id  { get; set; }
   public string UserName { get; set; }
+  public byte[] PasswordHash { get; set; }
+
+  public byte[] PasswordSalt { get; set; }
 }
