9c88a6427ea6aed20db226452fd4537246606d10
André Vital <andre@andrevital.com>
2023-10-04 16:59:48 -0600

    Add auth middleware to users controller

--- packages/backend/Controllers/UsersController.cs
+++ packages/backend/Controllers/UsersController.cs
@@ -1,21 +1,25 @@
 using backend.Data;
 using backend.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers;
 
+[Authorize]
 public class UsersController : BaseController  {
   private readonly DataContext _context;
   public UsersController(DataContext context) {
     _context = context; 
   }
 
+  [AllowAnonymous]
   [HttpGet]
   public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
     return await _context.Users.ToListAsync();
   }
 
+  [Authorize]
   [HttpGet("{id}")]
   public async Task<ActionResult<AppUser>> GetUser(int id) {
     return await _context.Users.FindAsync(id);
