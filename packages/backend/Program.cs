de6dde698c6617ac250f0a8a279be36c22437123
André Vital <andre@andrevital.com>
2023-10-04 16:51:48 -0600

    Added extensions to main program file

--- packages/backend/Program.cs
+++ packages/backend/Program.cs
@@ -1,15 +1,12 @@
-using backend.Data;
-using Microsoft.EntityFrameworkCore;
+using backend.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options => {
-  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,6 +14,7 @@ var app = builder.Build();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
