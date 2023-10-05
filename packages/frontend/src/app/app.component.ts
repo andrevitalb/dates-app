cd0677f0059d306b95d1cc5b0f0cf7e868951afa
André Vital <andre@andrevital.com>
2023-10-05 13:33:50 -0600

    Expanded app component with propagated changes

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from "@angular/common/http"
 import { Component, OnInit } from "@angular/core"
+import { IUser } from "models/user"
+import { AccountService } from "./services/account.service"
 
 @Component({
 	selector: "app-root",
@@ -8,15 +9,17 @@ import { Component, OnInit } from "@angular/core"
 })
 export class AppComponent implements OnInit {
 	title = "Dates App"
-	users: any
 
-	constructor(private http: HttpClient) {}
+	constructor(private accountService: AccountService) {}
 
 	ngOnInit(): void {
-		this.http.get("https://localhost:5001/api/users").subscribe({
-			next: (response) => (this.users = response),
-			error: (err) => console.error(err),
-			complete: () => console.log("Request completed!"),
-		})
+		this.setCurrentUser()
+	}
+
+	setCurrentUser(): void {
+		const userString = localStorage.getItem("user")
+		if (!userString) return
+		const user: IUser = JSON.parse(userString)
+		this.accountService.setCurrentUser(user)
 	}
 }
