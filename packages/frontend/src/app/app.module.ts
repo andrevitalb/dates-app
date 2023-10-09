c2085e3f9168f86bd95c749644dfdddbef4e9c42
André Vital <andre@andrevital.com>
2023-10-09 08:04:32 -0600

    Added extra components to app module

--- packages/frontend/src/app/app.module.ts
+++ packages/frontend/src/app/app.module.ts
@@ -3,15 +3,28 @@ import { NgModule } from "@angular/core"
 import { FormsModule } from "@angular/forms"
 import { BrowserModule } from "@angular/platform-browser"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { AppRoutingModule } from "app-routing.module"
+import { AppComponent } from "app.component"
+import { HomeComponent } from "components/home/home.component"
+import { ListsComponent } from "components/lists/lists.component"
+import { MemberDetailComponent } from "components/members/member-detail/member-detail.component"
+import { MemberListComponent } from "components/members/member-list/member-list.component"
+import { MessagesComponent } from "components/messages/messages.component"
+import { NavComponent } from "components/nav/nav.component"
+import { RegisterComponent } from "components/register/register.component"
 import { BsDropdownModule } from "ngx-bootstrap/dropdown"
-import { AppRoutingModule } from "./app-routing.module"
-import { AppComponent } from "./app.component"
-import { HomeComponent } from "./components/home/home.component"
-import { NavComponent } from "./components/nav/nav.component";
-import { RegisterComponent } from './components/register/register.component'
 
 @NgModule({
-	declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+	declarations: [
+		AppComponent,
+		NavComponent,
+		HomeComponent,
+		RegisterComponent,
+		MemberListComponent,
+		MemberDetailComponent,
+		ListsComponent,
+		MessagesComponent,
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
