dbd01359fc4dfda4d2dd0db83915441b119466e9
André Vital <andre@andrevital.com>
2023-10-10 09:04:54 -0600

    Added error management for login

--- packages/frontend/src/app/components/nav/nav.component.ts
+++ packages/frontend/src/app/components/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core"
 import { Router } from "@angular/router"
 import { IUser } from "models/user"
+import { ToastrService } from "ngx-toastr"
 import { Observable, of } from "rxjs"
 import { AccountService } from "services/account.service"
 
@@ -16,22 +17,30 @@ export class NavComponent implements OnInit {
 	constructor(
 		private accountService: AccountService,
 		private router: Router,
+		private toastr: ToastrService,
 	) {}
 
 	ngOnInit(): void {
 		this.currentUser$ = this.accountService.currentUser$
+		this.resetFormFields()
+	}
+
+	resetFormFields(): void {
+		this.model = {
+			username: "",
+			password: "",
+		}
 	}
 
 	login(): void {
 		this.accountService.login(this.model).subscribe({
 			next: () => this.router.navigateByUrl("/members"),
-			error: (error) => {
-				console.log(error)
-			},
+			error: (error) => this.toastr.error(error.error),
 		})
 	}
 
 	logout(): void {
+		this.resetFormFields()
 		this.accountService.logout()
 		this.router.navigateByUrl("/")
 	}
