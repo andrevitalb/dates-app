47467e13bd1c0f775cfb4a8e0cd5eef9fa3d6a57
André Vital <andre@andrevital.com>
2023-11-10 12:11:45 -0600

    Create authGuard

---
+++ packages/frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { inject } from "@angular/core"
+import { CanActivateFn } from "@angular/router"
+import { ToastrService } from "ngx-toastr"
+import { map } from "rxjs"
+import { AccountService } from "services/account.service"
+
+export const authGuard: CanActivateFn = (route, state) => {
+	const accountService = inject(AccountService)
+
+	const toastr = inject(ToastrService)
+
+	return accountService.currentUser$.pipe(
+		map((user) => {
+			if (user) return true
+			else {
+				toastr.error("Debes ingresar al sistema")
+
+				return false
+			}
+		}),
+	)
+}
