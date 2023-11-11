42d31d166c65d087ff603eece4c694acd2d491f6
André Vital <andre@andrevital.com>
2023-11-10 18:43:42 -0600

    Create AutoMapper helper for BE

---
+++ packages/backend/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using backend.DTOs;
+using backend.Entities;
+using backend.Extensions;
+
+namespace backend.Helpers;
+
+public class AutoMapperProfiles: Profile {
+  public AutoMapperProfiles() {
+    CreateMap<AppUser, MemberDto>()
+      .ForMember(dest => dest.PhotoUrl,
+        opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+      .ForMember(dest => dest.Age,
+        opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+    CreateMap<Photo, PhotoDto>();
+  }
+}
