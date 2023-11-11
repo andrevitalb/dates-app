0a7b0886eed0120c2a7c9bbe0325faf68fd1d19f
André Vital <andre@andrevital.com>
2023-11-10 18:55:46 -0600

    Fetch members for members list component

--- packages/frontend/src/app/components/members/member-list/member-list.component.ts
+++ packages/frontend/src/app/components/members/member-list/member-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { IMember } from "interfaces/member"
+import { MembersService } from "services/members.service"
 
 @Component({
-  selector: 'app-member-list',
-  templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.scss']
+	selector: "app-member-list",
+	templateUrl: "./member-list.component.html",
+	styleUrls: ["./member-list.component.scss"],
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+	members: IMember[] = []
 
+	constructor(private membersService: MembersService) {
+		this.loadMembers()
+	}
+
+	loadMembers() {
+		this.membersService.getMembers().subscribe({
+			next: (members) => (this.members = members),
+		})
+	}
+
+	ngOnInit(): void {}
 }
